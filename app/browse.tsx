6ef9f3055e03ad4cfc98e6cc6baf3e081f9adab5
Ikenna Richard <oguejioforrichard@gmail.com>
2025-11-02 16:19:26 +0100

    refactor: add category route to the browse screen

--- app/browse.tsx
+++ app/browse.tsx
@@ -2,11 +2,13 @@ import CategoryCard from "@/components/CategoryCard";
 import GradientText from "@/components/GradientText";
 import Toggle from "@/components/Toggle";
 import { categories } from "@/utils/data";
+import { useRouter } from "expo-router";
 import { useState } from "react";
 import { ScrollView, Text, View } from "react-native";
 
 export default function BrowseScreen() {
   const [view, setView] = useState<any>("grid");
+  const router = useRouter()
   return (
     <ScrollView className="flex-1 " showsVerticalScrollIndicator={false}>
       <View className="px-4 pt-6 pb-4 gap-2">
@@ -25,8 +27,10 @@ export default function BrowseScreen() {
           <CategoryCard
             key={category.id}
             category={category}
-            onPress={() => console.log("Category pressed:", category.title)}
-            variant={view === "grid" ? 'flat' : "split"}
+            onPress={router.push(
+              `/category/${category.id}?name=${category.title}`
+            )}
+            variant={view === "grid" ? "flat" : "split"}
           />
         ))}
       </View>
