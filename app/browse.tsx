356bc7501440fe5eb3710727fe24a4cfd5e0642a
Ikenna Richard <oguejioforrichard@gmail.com>
2025-11-02 16:07:58 +0100

    feat: add browse categorie screen

---
+++ app/browse.tsx
@@ -0,0 +1,35 @@
+import CategoryCard from "@/components/CategoryCard";
+import GradientText from "@/components/GradientText";
+import Toggle from "@/components/Toggle";
+import { categories } from "@/utils/data";
+import { useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+
+export default function BrowseScreen() {
+  const [view, setView] = useState<any>("grid");
+  return (
+    <ScrollView className="flex-1 " showsVerticalScrollIndicator={false}>
+      <View className="px-4 pt-6 pb-4 gap-2">
+        <View className="py-4">
+          <GradientText text="Browse Categories" />
+        </View>
+        <Text className="text-gray-600 text-base leading-6 font-poppins-regular">
+          Explore our curated collections of stunning wallpapers
+        </Text>
+        <View className="my-1">
+          <Toggle view={view} onViewChange={setView} />
+        </View>
+      </View>
+      <View className="py-4 gap-5">
+        {categories.map((category) => (
+          <CategoryCard
+            key={category.id}
+            category={category}
+            onPress={() => console.log("Category pressed:", category.title)}
+            variant={view === "grid" ? 'flat' : "split"}
+          />
+        ))}
+      </View>
+    </ScrollView>
+  );
+}
